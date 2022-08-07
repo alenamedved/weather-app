80bd3b29637eeceb89adabe3e34bbe9c63ef53bd
Alena <83471057+alenamedved@users.noreply.github.com>
2022-08-06 20:33:26 -0600

    added input validation, mui textfields with error messages

--- src/App.js
+++ src/App.js
@@ -1,59 +1,93 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import {
+  Chip,
+  Grid,
+  TextField,
+  Typography,
+  Button,
+  Paper,
+  Container,
+  Tooltip,
+} from "@mui/material";
+import { Box } from "@mui/system";
 
 function App() {
   const [lat, setLat] = useState("");
   const [long, setLong] = useState("");
 
-  const [weatherData, setWeatherData] = useState('')
-const [warnign, setWarning] = useState('')
+  const [weatherData, setWeatherData] = useState([]);
+  const [location, setLocation] = useState("");
+  const [warnign, setWarning] = useState("");
 
   const url = `https://api.weather.gov/points/${lat},${long}`;
-const regex = "^(-?\\d+(?:\\.\\d+)?),(-?\\d+(?:\\.\\d+)?)$"
-
+  const latitude = "^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)$"; // [-90,90]
+  const longitude = "^[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)$"; // [-180,180]
 
   //set the values of lat and long
   const handleChange = (e) => {
-    if (e.target.name === "latitude") {
-      setLat(e.target.value);
-    } else {
-      setLong(e.target.value);
+    const { name, value } = e.target;
+    switch (name) {
+      case "latitude":
+        setLat(value);
+        break;
+      case "longitude":
+        setLong(value);
+        break;
+      default:
+        break;
     }
+  };
+  console.log(lat, long);
+
+  //validate an input for long and lat
+  const inputValidation = (pattern, value) => {
+    console.log(value.length);
+    if (value.length > 0) {
+      const regex = new RegExp(pattern);
+      const matches = regex.test(value);
+      return matches;
+    }
+    return true;
   };
 
   const handleSubmitForm = async (e) => {
-    console.log('fetch data')
     e.preventDefault();
     await axios
       .get(url)
       .then((result) => {
-        console.log(result, 'result')
-        if(result.data.properties.forecast) { //send another request if valid response is received
+        // console.log(result, "result");
+        // console.log(result.data.properties.relativeLocation.properties, "result.data.properties.relativeLocation.properties");
+        setLocation({
+          city: result.data.properties.relativeLocation.properties.city,
+          state: result.data.properties.relativeLocation.properties.state,
+        });
+        if (result.data.properties.forecast) {
+          //send another request if valid response is received
           axios
             .get(result.data.properties.forecast)
             .then((data) => {
-              console.log(data.data.properties.periods, 'data prop')
-              // setWeatherData(data.data.properties.periods)
+              // console.log(data.data.properties.periods, "data prop");
+              setWeatherData(data.data.properties.periods);
             })
-            .catch(error => console.log(error, 'error fetching the weather'))
-
-        } else if(result.data.title === "Not Found") {
-          setWarning("Sorry, we coulnd't retrieve location's data ")
+            .catch((error) => console.log(error, "error fetching the weather"));
+        } else if (result.data.title === "Not Found") {
+          setWarning("Sorry, we coulnd't retrieve location's data ");
         }
-  })
-      .catch(error => {
-        
-        setWarning(error.response.data.title)
-        setTimeout(()=> { //clear the message in 5 sec
-          setWarning('')
-        }, 5000)
-        console.log(error, 'error receiving a link')
+      })
+      .catch((error) => {
+        setWarning(error.response.data.detail);
+        setTimeout(() => {
+          //clear the message in 5 sec
+          setWarning("");
+        }, 5000);
+        console.log(error, "error receiving a link");
       });
 
     console.log(lat, "lat", long, "long");
-    setLat('') //reset the values
-    setLong('') //reset the values
+    setLat(""); //reset the values
+    setLong(""); //reset the values
   };
 
   const getCurrentLocation = () => {
@@ -64,29 +98,54 @@ const regex = "^(-?\\d+(?:\\.\\d+)?),(-?\\d+(?:\\.\\d+)?)$"
       setLong(position.coords.longitude);
     });
   };
-console.log(weatherData, 'weather')
+
+  const displayWeatherForecast = (array) => {
+    return array.map((period) => (
+      <div key={period.number}>
+        <p>{period.detailedForecast}</p>
+        <img src={period.icon} alt="icon" />
+      </div>
+    ));
+  };
+  console.log(weatherData, "weather");
+  console.log(location, "location");
   return (
     <div className="App">
-    {warnign && <div>{warnign}</div>}
+      {warnign && <div>{warnign}</div>}
       <form type="submit" onSubmit={(e) => handleSubmitForm(e)}>
-        <input
-          type="number"
-          name="latitude"
-          onChange={(e) => handleChange(e)}
-          value={lat}
-          placeholder="latitude"
-        />
-        <input
-          type="number"
-          name="longitude"
-          pattern='d\+\.\d\d$'
-          onChange={(e) => handleChange(e)}
-          value={long}
-          placeholder="longtitude"
-        />
+        <Box noValidate>
+          <TextField
+            type="number"
+            name="latitude"
+            placeholder="latitude"
+            label="latitude"
+            required
+            size="small"
+            InputLabelProps={{ shrink: true }}
+            value={lat}
+            onChange={(e) => handleChange(e)}
+            error={!inputValidation(latitude, lat)}
+            helperText={!inputValidation(latitude, lat) && "Latitude should be in '-90.00'-'90.00' range"}
+          />
+          <TextField
+            type="number"
+            name="longitude"
+            placeholder="longitude"
+            label="longitude"
+            required
+            size="small"
+            InputLabelProps={{ shrink: true }}
+            value={long}
+            onChange={(e) => handleChange(e)}
+            error={!inputValidation(longitude, long)}
+            helperText={!inputValidation(longitude, long) && "Longitude should be in '-180.00'-'180.00' range"}
+          />
+        </Box>
+        
         <button>Get the weather</button>
       </form>
       <button onClick={getCurrentLocation}>Get current coordinates</button>
+      {displayWeatherForecast(weatherData)}
     </div>
   );
 }
